b98812cc3d8ca768cdb594fdc0e4abe50b12d427
Aneesh Mulye <851233+aneesh-mulye@users.noreply.github.com>
2026-07-25 04:33:33 +0530

    Chapter 2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::io::{self, Read};
+use crossterm::terminal::{enable_raw_mode, disable_raw_mode};
+
 fn main() {
-    println!("Goodbye, unworld!");
+    enable_raw_mode().unwrap();
+
+    for b in io::stdin().bytes() {
+        match b {
+            Ok(b) => {
+                let c = b as char;
+                println!("Binary: {0:08b} ASCII: {0:#03}{1}\r", b,
+                    if !c.is_control() {
+                        format!(" Character: {0:#?}", c)
+                    } else {
+                        String::from("")
+                    });
+                if c == 'q' {
+                    break;
+                }
+            }
+            Err(err) => {
+                println!("Error: {}", err);
+            }
+        }
+    }
+
+    disable_raw_mode().unwrap();
 }
